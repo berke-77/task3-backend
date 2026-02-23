2e4d0761b6bff4be74cb7e2592fb8481eba61d61
berke-77 <Berke_kas@outlook.com>
2026-02-23 15:47:46 +0300

    Update Category.java

--- src/main/java/com/example/demo/entities/concretes/Category.java
+++ src/main/java/com/example/demo/entities/concretes/Category.java
@@ -13,11 +13,21 @@ public class Category {
     @Column(nullable = false)
     private String name;
 
-    public Category() {}
+    public Category() {
+        
+    }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() { 
+        return id; 
+    }
+    public void setId(int id) { 
+        this.id = id;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() { 
+        return name; 
+    }
+    public void setName(String name) { 
+        this.name = name;
+    }
 }

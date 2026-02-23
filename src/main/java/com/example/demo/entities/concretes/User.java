472b5c7564e3c3a8e6ee57cf119310cbab1cfc17
berke-77 <Berke_kas@outlook.com>
2026-02-23 15:46:29 +0300

    Update User.java

--- src/main/java/com/example/demo/entities/concretes/User.java
+++ src/main/java/com/example/demo/entities/concretes/User.java
@@ -23,20 +23,42 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    public User() {}
+    public User() {
+        
+    }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() { 
+        return id; 
+    }
+    public void setId(int id) {
+        this.id = id; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email; 
+    }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getUsername() { 
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username; 
+    }
 
-    public Role getRole() { return role; }
-    public void setRole(Role role) { this.role = role; }
+    public Role getRole() {
+        return role; 
+    }
+    public void setRole(Role role) {
+        this.role = role; 
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password; 
+    }
 }

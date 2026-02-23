9f2c5d94c22b3422b6f8c25567154e82fd6b717f
berke-77 <Berke_kas@outlook.com>
2026-02-23 15:45:30 +0300

    Update Role.java

--- src/main/java/com/example/demo/entities/concretes/Role.java
+++ src/main/java/com/example/demo/entities/concretes/Role.java
@@ -13,11 +13,21 @@ public class Role {
     @Column(nullable = false, unique = true)
     private String name;
 
-    public Role() {}
+    public Role() {
+        
+    }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getId() { 
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id; 
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
